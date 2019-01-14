325de8729560e4c8d081c4ccf4241ec2da094498
Ryo Suzuki <euphsuzuki@hotmail.com>
2019-01-14 23:29:15 +0900

    GUIでステップ実行可能にした

--- RISC-V_CPU_Emulator/InstructionViewer.cs
+++ RISC-V_CPU_Emulator/InstructionViewer.cs
@@ -1,28 +1,74 @@
 ﻿using RV32_Cpu;
 using RV32_Lsu;
+using RV32_Lsu.Constants;
+using RV32_Lsu.MemoryHandler;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static RISC_V_CPU_Emulator.InstructionConverter;
+using static RV32_Cpu.InstructionConverter;
 
 namespace RISC_V_CPU_Emulator {
     public partial class InstructionViewer : Form {
+        RV32_HaedwareThread cpu;
         public InstructionViewer(RV32_HaedwareThread cpu) {
+            this.cpu = cpu;
+            string dirPath = @"..\..\..\..\riscv-tests\build\isa\";
+            this.cpu.LoadProgram(dirPath + "rv32mi-p-illegal");
+            this.cpu.registerSet.Mem.HostAccessAddress.Add(this.cpu.GetToHostAddr());
+
+
             InitializeComponent();
+
+            this.DumpViewTextBox.Text = File.ReadAllText(dirPath + "rv32mi-p-illegal" + ".dump");
+
+            string mode = Enum.GetName(typeof(PrivilegeLevels), cpu.registerSet.CurrentMode);
+
+            UpdateTextLabels(mode, cpu.registerSet.PC, cpu.registerSet.IR);
+            ChangeColorCurrentStep(cpu.registerSet.PC);
+
+            this.Update();
         }
 
-        public void UpdateView(uint v) {
+        /// <summary>
+        /// テキストラベルを更新する
+        /// </summary>
+        /// <param name="pcValue">PCの値</param>
+        /// <param name="insValue">命令の値</param>
+        private void UpdateTextLabels(string mode, uint pcValue, uint insValue) {
 
-            RiscvInstruction ins = InstructionConverter.Decode(v);
+            RiscvInstruction ins = InstructionConverter.Decode(insValue);
 
-            this.BinaryInstructionLabel.Text = Convert.ToString(v, 2).PadLeft(32, '0');
+            // プログラムカウンタ
+            this.PCValueLabel.Text = "0x" + pcValue.ToString("X").PadLeft(8, '0');
+
+            // 命令レジスタ
             this.InstructionNameLabel.Text = ins.Name;
+
+            // 実行モード
+            this.CurrentModeLabel.Text = mode;
+
+            // 2進数の命令
+            if (ins.Name.StartsWith("C.")) {
+                this.BinaryInstructionLabel.Text = Convert.ToString(insValue, 2).PadLeft(16, '0').PadLeft(32, ' ');
+            } else {
+                this.BinaryInstructionLabel.Text = Convert.ToString(insValue, 2).PadLeft(32, '0');
+            }
+            this.BinaryInstructionLabel.Text = this.BinaryInstructionLabel.Text.Insert(25, " ").Insert(20, " ").Insert(17, " ").Insert(12, " ").Insert(7, " ");
+
+            if (immediateType.Keys.Contains(ins.InstructionType)) {
+                this.BinaryInstructionDigitLabel.Text = immediateType[ins.InstructionType];
+            } else {
+                this.BinaryInstructionDigitLabel.Text = "";
+            }
+
 
             Label[] argLabels = new Label[] {
                 this.Arg1Name,
@@ -40,30 +86,97 @@ namespace RISC_V_CPU_Emulator {
             int i = 0;
             foreach (string key in ins.Arguments.Keys) {
                 argLabels[i++].Text = key;
-                argLabels[i++].Text = ins.Arguments[key].ToString();
+
+                // 整数レジスタ名
+                if (key.StartsWith("rs") || key.StartsWith("rd")) {
+                    argLabels[i++].Text = IntergerRegisterName[(int)ins.Arguments[key]];
+
+                    // 浮動小数点レジスタ名
+                } else if (key.StartsWith("frs") || key.StartsWith("frd")) {
+                    argLabels[i++].Text = FloatPointRegisterName[(int)ins.Arguments[key]];
+
+                    //コントロール・ステータスレジスタ名
+                } else if (key.Equals("csr")) {
+                    argLabels[i++].Text = Enum.GetName(typeof(CSR), (CSR)ins.Arguments[key]);
+
+                } else if (key.Equals("immediate") && ins.InstructionType.Equals("J")) {
+                    argLabels[i++].Text = "0x" + ins.Arguments[key].ToString("X").PadLeft(5, '0');
+
+                } else if (key.Equals("immediate") && (ins.InstructionType.Equals("B") || ins.InstructionType.Equals("S"))) {
+                    argLabels[i++].Text = "0x" + ins.Arguments[key].ToString("X").PadLeft(3, '0');
+
+                } else {
+                    argLabels[i++].Text = ins.Arguments[key].ToString();
+                }
             }
-            while (i < 10) {
+            while (i < argLabels.Length) {
                 argLabels[i++].Text = "";
             }
+
+        }
+        /// <summary>
+        /// 指定したPCの行の背景色を変更する
+        /// </summary>
+        /// <param name="pcValue">PCの値</param>
+        private void ChangeColorCurrentStep(uint pcValue) {
+            RichTextBox dump = this.DumpViewTextBox;
+            string t = dump.Text.ToLower();
+
+            string pc = (pcValue.ToString("X").PadLeft(8, '0') + ":").ToLower();
+
+            int startIdx = t.IndexOf("\n" + pc) + 1;
+            if (startIdx < 1) return;
+
+            int startLineCount = (startIdx - t.Substring(0, startIdx).Replace("\n", "").Length);
+
+            int length = t.IndexOf("\n", startIdx) - startIdx;
+            if (length <= 0) return;
+
+            dump.Select(0, t.Length);
+            dump.SelectionBackColor = Color.White;
+
+            dump.Select(startIdx, length);
+            dump.SelectionBackColor = Color.LightPink;
+            dump.Select(startIdx - 1, 1);
+            dump.ScrollToCaret();
+        }
+
+        private void StepExecuteButton_Click(object sender, EventArgs e) {
+
+            try {
+                // プログラムを1ステップ実行
+                cpu.StepExecute();
+
+                string mode = Enum.GetName(typeof(PrivilegeLevels), cpu.registerSet.CurrentMode);
+                UpdateTextLabels(mode, cpu.registerSet.PC, cpu.registerSet.IR);
+                ChangeColorCurrentStep(cpu.registerSet.PC);
+
+            } catch (HostAccessTrap) {
+
+            }
+
 
             this.Update();
         }
 
-        public void Test() {
-            this.Arg1Name.Text = "arg1";
-            this.Arg1Value.Text = "value1value1";
-            this.Arg2Name.Text = "arg2";
-            this.Arg2Value.Text = "value2value2";
-            this.Arg3Name.Text = "arg3";
-            this.Arg3Value.Text = "value3value3";
-            this.Arg4Name.Text = "arg4";
-            this.Arg4Value.Text = "value4value4";
-            this.Arg5Name.Text = "arg5";
-            this.Arg5Value.Text = "value5value5";
-        }
-
-        private void ExecuteButton_Click(object sender, EventArgs e) {
-            Test();
-        }
+        /// <summary>
+        /// 
+        /// </summary>
+        private static readonly IReadOnlyDictionary<string, string> immediateType = new ReadOnlyDictionary<string, string>(new Dictionary<string, string>() {
+            { "R",       "--------Opcode-------  -------rs2--------    -------rs1-------  -Opcide-    ------rd--------    -------Opcode--------" },
+            { "I",       "----------------Immediate---------------    -------rs1-------   -Opcide-    ------rd--------    -------Opcode--------" },
+            { "S",       "------Immediate------   -------rs2--------   -------rs1-------   -Opcide-   ---Immediate---    -------Opcode--------" },
+            { "B",       "------Immediate------   -------rs2--------   -------rs1-------   -Opcide-   ---Immediate---    -------Opcode--------" },
+            { "U",       "-------------------------------Immediate-------------------------------    ------rd--------    -------Opcode--------" },
+            { "J",       "-------------------------------Immediate-------------------------------    ------rd--------    -------Opcode--------" },
+            { "SHIFT",   "------Opcode---------  ------shamt------    -------rs1-------   -Opcide-    ------rd--------    -------Opcode--------" },
+            { "CSR",     "--------------------CSR------------------    -------rs1-------   -Opcide-    ------rd--------    -------Opcode--------" },
+            { "R4",      "-------rs3------- -Op- --------rs2-------    -------rs1-------   -Opcode-    ------rd--------    -------Opcode--------" },
+            { "RF",      "--------Opcode-------  -------rs2--------  -------rs1--------  ---rm---    ------rd--------    -------Opcode--------" },
+            { "NONE",    "-----------------------------------------------------OpCode-------------------------------------------------------------" },
+            { "FENCE",   "-----------------------------------------------------OpCode-------------------------------------------------------------" },
+            { "FMV",     "------------------Opcode-----------------  -------rs1-------   -Opcode-    ------rd--------    -------Opcode--------" },
+            { "A",       "----Opcode---- -aq/rl- --------rs2-------  -------rs1-------   -Opcode-    ------rd--------    -------Opcode--------" },
+        });
     }
 }
--- RISC-V_CPU_Emulator/Program.cs
+++ RISC-V_CPU_Emulator/Program.cs
@@ -19,69 +19,12 @@ namespace RISC_V_CPU_Emulator {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            string dirPath = @"..\..\..\..\riscv-tests\build\isa\";
-
-            Regex rex;
-            rex = new Regex("rv32[msu][a-z]-p-[a-z_]+$", RegexOptions.Compiled);
-            //rex = new Regex("rv32ua-p-lrsc$", RegexOptions.Compiled);
-
-            string[] files = Directory.GetFiles(dirPath).Where<string>(
-                f => (rex.IsMatch(f))).ToArray();
 
             byte[] mem = new byte[32 * 1024];
 
-            RV32_HaedwareThread cpu = new RV32_HaedwareThread("AMCFD", mem);
+            RV32_HaedwareThread cpu = new RV32_HaedwareThread("IAMFDC", mem);
 
             Application.Run(new InstructionViewer(cpu));
-
-
-            Task.Run(() => {
-                RunRiscvProgram(cpu, files);
-            });
-        }
-
-        private static void RunRiscvProgram(RV32_HaedwareThread cpu, string[] RiscvBinaryFiles) {
-
-            float all = RiscvBinaryFiles.Length;
-            float successed = 0f;
-
-            foreach (string file in RiscvBinaryFiles) {
-
-                Console.Write(Path.GetFileName(file).PadRight(20));
-
-                int rCode = 0;
-                rCode = cpu.LoadProgram(file);
-
-                cpu.registerSet.Mem.HostAccessAddress.Add(cpu.GetToHostAddr());
-
-                string errstr = null;
-                while (true) {
-                    try {
-                        rCode = cpu.Run();
-                        if (rCode == 0) {
-                            break;
-                        }
-                    } catch (HostAccessTrap) {
-                        break;
-                    }
-                }
-                if (rCode != 0) {
-                    Console.WriteLine(" " + cpu.registerSet.PC.ToString("X") + " : プログラム異常終了");
-                    break;
-                }
-
-                if (cpu.registerSet.GetValue(Register.gp) == 1) {
-                    Console.WriteLine(" : テスト成功");
-                    successed++;
-                } else if (cpu.registerSet.GetValue(Register.gp) != 0) {
-                    Console.WriteLine(" : テスト失敗 : test_" + (cpu.registerSet.GetValue(Register.gp) >> 1));
-                } else if (errstr != null) {
-                    Console.WriteLine(" : エラー     :" + errstr);
-                }
-            }
-            Console.WriteLine("成功  : " + successed.ToString("#,0"));
-            Console.WriteLine("失敗  : " + (all - successed).ToString("#,0"));
-            Console.WriteLine("成功率: " + ((successed / all) * 100f).ToString("#,0.00") + "%");
         }
     }
 }
